57de9c3bb6194a8edbba443425af94e44be06cf7
xSenny <ursuarsenie@gmail.com>
2024-03-07 16:01:26 +0200

    Modified the connectToDb function

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -9,7 +9,7 @@ export const connectToDatabase = async () => {
 
     if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
+        dbName: 'eTurist',
         bufferCommands: false,
     })
 
